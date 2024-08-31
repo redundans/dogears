5ae06ecb502b2687e9ef6d6a897eba9fee6ad968
redundans <jesper@klandestino.se>
2024-08-31 16:27:05 +0200

    Update title.

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,5 +2,5 @@
 <meta name="description" content="">
 <meta name="Saquib" content="Blade">
 <link href="https://fonts.cdnfonts.com/css/jetbrains-mono-2" rel="stylesheet">
-<title>Links</title>
+<title>Turist/Links</title>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
